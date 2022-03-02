94edad1f42de476445cd8acbcaa5d169c13bc94f
John <ashton.basile@gmail.com>
2022-03-02 09:16:21 +0100

    Added some quick practice with arrays in node.

---
+++ array_practice.js
@@ -0,0 +1,7 @@
+var names = [];
+
+names.push("Bob");
+
+names.push("Sven","Robert","Hidalgo","John");
+
+console.log(`The third name in the list is: ${names[2]}`);
--- typeScript_practice.js
+++
@@ -1,6 +0,0 @@
-function compact(arr) {
-    if (arr.length > 10)
-        return arr.slice(0, 10);
-    return arr;
-}
-console.log(compact(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']));
